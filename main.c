9d108f87fea4bc0fd132bcffe841530f8246b295
Otavio <correaotavio0409@gmail.com>
2024-09-13 17:03:38 -0300

    Otimizando Funcoes

--- main.c
+++ main.c
@@ -3,41 +3,38 @@
 
 struct Date //dados que serão setados
 {
-    int day;
-    int month;
-    int year;
-    int valid;//Usado para verificar se a data é válida ou não
+    short day;
+    short month;
+    short year;
+    short valid;//Usado para verificar se a data é válida ou não
 };
 
-struct Date initDate() //utilizando o conceito de verdadeiro ou falso
+void initDate(struct Date *dt) //utilizando o conceito de verdadeiro ou falso
 {
-    struct Date dt;
-    dt.valid = 0;
-    return dt;
+    dt->valid = 0;
 }
 
-struct Date changeData(struct Date dt, int day, int month, int year)//verificando dados validos
+void changeData(struct Date *dt, int day, int month, int year)//verificando dados validos
 {
-    dt.day = day;
-    dt.month = month;
-    dt.year = year;
+    dt->day = day;
+    dt->month = month;
+    dt->year = year;
 
-    if ((dt.day >= 1 && dt.day <= 31) &&
-        (dt.month >= 1 && dt.month <= 12) &&
-        (dt.year >= 1900 && dt.year <= 2100))
+    if ((dt->day >= 1 && dt->day <= 31) &&
+        (dt->month >= 1 && dt->month <= 12) &&
+        (dt->year >= 1900 && dt->year <= 2100))
     {
-        dt.valid = 1; //return true;
+        dt->valid = 1; //return true;
     }
     else{
-        dt.valid = 0; //return false;
+        dt->valid = 0; //return false;
     }
-    return dt;
 }
 
-void printDate(struct Date dt)//chamando a função Date dt
+void printDate(const struct Date *dt)//chamando a função Date dt
 {
-    if(dt.valid){
-        printf("%02d/%02d/%d\n", dt.day, dt.month, dt.year);//printado data válida ou inválido
+    if(dt->valid){
+        printf("%02d/%02d/%d\n", dt->day, dt->month, dt->year);//printado data válida ou inválido
     }else{
         printf("Invalid Date!\n");
     }
@@ -45,10 +42,13 @@ void printDate(struct Date dt)//chamando a função Date dt
 
 int main()
 {
-    struct Date pgt = initDate();
+    struct Date pgt;
+    initDate(&pgt);
 
-    pgt = changeData(pgt, 12,11,2099);
-    printDate(pgt); //chamando a função printDate
+    changeData(&pgt, 12,12,2099);
+    printDate(&pgt); //chamando a função printDate
+
+    printf("Size: %llu\n", sizeof(pgt));
 
     return 0;
 }
